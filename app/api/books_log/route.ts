7f3167ec0866e96d82480ce41dc528ceb380214c
J. <turned.to.a.dead.channel@gmail.com>
2026-06-02 09:40:08 -0400

    Fix log logic

--- app/api/books_log/route.ts
+++ app/api/books_log/route.ts
@@ -1,9 +1,8 @@
 import { postBookLog } from '@/lib/queries/bookslog'
-import { dates } from "@/lib/dates";
 import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
   const { uid, startPage, endPage, quote } = await request.json()
-  await postBookLog({ uid, dateLogged: dates.todayRaw, startPage, endPage, quote })
+  await postBookLog({ uid, dateLogged: new Date(), startPage, endPage, quote })
   return NextResponse.json({ success: true })
 }
